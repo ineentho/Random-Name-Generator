06ab528420d2460b18ccf26c92aab1d9441e498b
Henrik <hkhenrik@gmail.com>
2011-10-26 22:08:11 +0200

    Added some basic functionallity

--- bg.js
+++ bg.js
@@ -1,4 +1,9 @@
 chrome.contextMenus.create({"title": "Generate random name", "contexts":["editable"], "onclick": genName});
-function genName(){
-	alert("Gen name");
+function genName(info, tab){
+	name=getName();
+	chrome.tabs.executeScript(null,{code:"document.activeElement.value+='"+name+"';"})
+}
+function getName(){
+	name = "Ineentho";
+	return name;
 }
