9886580bdc12c19a8a26727bb2d7f9cfe6a57494
Henrik <hkhenrik@gmail.com>
2011-10-28 22:21:10 +0200

    Added name mode

--- optionsjs.js
+++ optionsjs.js
@@ -1,3 +1,20 @@
 $(function() {
-	$( "#accordion" ).accordion();
+	$( "#accordion" ).accordion({
+		autoHeight: false,
+		navigation: true});
+	$( "#slider" ).slider({
+		value:1,
+		min: 1,
+		max: 3,
+		step: 1,
+		slide: function( event, ui ) {
+			if(ui.value==1)
+				$( "#amount" ).val( "Simple");
+			if(ui.value==2)
+				$( "#amount" ).val( "Moderate");
+			if(ui.value==3)
+				$( "#amount" ).val( "Advanced");
+		}
+	});
+	$( "#amount" ).val( "Simple" );
 });
