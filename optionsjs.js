65419b3bcae00fbd12fe51c7a18217d9a22193a8
Henrik <hkhenrik@gmail.com>
2011-10-29 11:47:28 +0200

    Added beginning of persistence

--- optionsjs.js
+++ optionsjs.js
@@ -1,11 +1,21 @@
 $(function() {
+	nType=getOpt("names.type");
+	inputChange("f",nType)
 	$( "#tabs" ).tabs();
-	$("input[name='hn']").change(function(){
+	$("input[name='hn']").change(inputChange);
+	function inputChange(f,v){
 		$("#predefinedOptions").slideUp();
 		$("#nameOptions").slideUp();
 		$("#customOptions").slideUp();
-		$("#"+$(this).val()).slideDown();
-	});
+		if(f=="f"){
+			$("#"+v).slideDown();
+			$("input[value='"+v+"'][name='hn']").attr("checked","checked");
+		}
+		else{
+			$("#"+$(this).val()).slideDown();
+			setOpt("names.type",$(this).val());
+		}
+	}
 	$("#predefinedNameType").change(function(){
 		if($(this).val()=="popular")
 			$("#popularInfo").slideDown();
--- pres.js
+++ pres.js
@@ -1,6 +1,17 @@
 function getOpt(opt){
-	
+	if(localStorage[opt]!=undefined)
+		return localStorage[opt];
+	else{
+		switch(opt){
+			case "names.predefined.type":
+				return "random";
+				break;
+			case "names.type":
+				return "predefinedOptions";
+				break;
+		}
+	}
 }
 function setOpt(opt, val){
-	
+	localStorage[opt]=val;
 }
