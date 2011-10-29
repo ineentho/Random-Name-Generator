aea9dc48f900a344b025b0428f8789e4d9303f73
Henrik <hkhenrik@gmail.com>
2011-10-29 09:36:36 +0200

    Added Tabs instead of the accordion

--- optionsjs.js
+++ optionsjs.js
@@ -1,20 +1,3 @@
 $(function() {
-	$( "#accordion" ).accordion({
-		autoHeight: false,
-		navigation: true});
-	$( "#slider" ).slider({
-		value:1,
-		min: 1,
-		max: 3,
-		step: 1,
-		slide: function( event, ui ) {
-			if(ui.value==1)
-				$( "#amount" ).val( "Simple");
-			if(ui.value==2)
-				$( "#amount" ).val( "Moderate");
-			if(ui.value==3)
-				$( "#amount" ).val( "Advanced");
-		}
-	});
-	$( "#amount" ).val( "Simple" );
+		$( "#tabs" ).tabs();
 });
